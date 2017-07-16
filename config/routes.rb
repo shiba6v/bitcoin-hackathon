f3e87ae9df029c1a30d9fcae83e719fd8ec28316
andoshin11 <shinglish11@gmail.com>
2017-07-16 15:10:42 +0900

    Send Result

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
   get '/api/v1/mining', to: 'blocks#start'
-  post '/api/v1', to: 'blocks#reload'
+  post '/api/v1/mining', to: 'blocks#reload'
   resources :blocks, only: :index
   resources :histories, only: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
