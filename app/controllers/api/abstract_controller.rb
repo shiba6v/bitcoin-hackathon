1808b5c337858b953c7859c3c13c4ce192f35c82
andoshin11 <shinglish11@gmail.com>
2017-07-18 01:14:17 +0900

    Add Miner API

---
+++ app/controllers/api/abstract_controller.rb
@@ -0,0 +1,2 @@
+class Api::AbstractController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,14 @@ Rails.application.routes.draw do
   resources :blocks, only: :index
   resources :histories, only: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :miners, only: [] do
+        collection do
+          get :index
+        end
+      end
+    end
+  end
 end
