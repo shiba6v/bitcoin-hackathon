fe6be88c8ba19bbbeb56ddc300fb9fa6bd3b7b10
andoshin11 <shinglish11@gmail.com>
2017-07-18 15:16:23 +0900

    Add current_session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def current_session
+
+    session_id = session[:miner_id]
+
+    if session_id.blank?
+      session[:miner_id] = SecureRandom.uuid
+      session_id = session[:miner_id]
+    end
+
+    session_id
+  end
 end
